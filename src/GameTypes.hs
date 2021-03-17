d0f25066395c6c59af5218aed222c0819f796b0d
rickshilling <rickshilling@penguin>
2021-03-16 23:23:53 -0400

    parameterizing

--- src/GameTypes.hs
+++ src/GameTypes.hs
@@ -5,20 +5,20 @@ module GameTypes
   Action(..),
   GameTree(..),
   InformationSets(..),
-  MyAction(..)
+  --MyAction(..)
   ) where
 import Control.Monad
 import Data.Map
 import Data.Set
 data Player = P1 | P2 | Chance deriving (Eq,Show) --Int
-data Action = Heads | Tails | ActionLeft | ActionRight | Forfeit deriving (Eq,Ord,Show) --Int
---data Action deriving (Eq,Ord,Show)
---data Player deriving (Eq,Ord,Show)
-data GameTree = GameNode {
-  rootLabel :: Player,
-  subForest :: [(Action, Maybe GameTree)]
-}
-type InformationSets = Map (Set Action) (Set [Action])
+--data Action = Heads | Tails | ActionLeft | ActionRight | Forfeit deriving (Eq,Ord,Show) --Int
 
-class MyAction a where
+class Action a where
   specificActions :: [a]
+
+type InformationSets a = Map (Set Action a) (Set [Action a])
+
+data GameTree a = GameNode {
+  rootLabel :: Player,
+  subForest :: [(Action a, Maybe GameTree a)]
+}
