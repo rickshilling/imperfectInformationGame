59aabdd507c7c636e64670126a99ec1e22af0fd8
rickshilling <rickshilling@penguin>
2021-03-19 19:57:49 -0400

    Cleaning

--- src/GameTypes.hs
+++ src/GameTypes.hs
@@ -8,16 +8,10 @@ module GameTypes
 import Control.Monad
 import Data.Map
 import Data.Set
-
 type InformationSets action = Map (Set action) (Set [action])
 data GameTree player action = GameNode {
   rootLabel :: player,
   subForest :: [(action, Maybe (GameTree player action))]
 }
 type History action = [action]
-
-{-
-class Sigma action where
-  value :: History action -> action -> Float
--}
 type Sigma action = History action -> action -> Float
