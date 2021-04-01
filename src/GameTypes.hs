d9d36e04b7eb1afafee9be10e2024a5ef95f1776
rickshilling <rickshilling@penguin>
2021-03-31 22:21:05 -0400

    reverting to type

--- src/GameTypes.hs
+++ src/GameTypes.hs
@@ -3,11 +3,8 @@ module GameTypes
   GameTree(..),
   History(..),
   Sigma(..),
-  InformationMap(..),
-  NewHistory(..),
-  NewChoices(..),
-  NewInformationSet(..),
-  NewInformationMap(..)
+  InformationSet(..),
+  InformationMap(..)
   ) where
 import Control.Monad
 import Data.Map
@@ -21,16 +18,3 @@ type History action = [action]
 type InformationSet action = Set (History action)
 type InformationMap action = Map (Set action) (InformationSet action)
 type Sigma action = History action -> action -> Float
-
-newtype NewHistory action = NewHistory [action] deriving (Eq, Show, Ord)
-
-newtype NewChoices action = NewChoices {
-  choices :: Set action
-  } deriving (Eq, Show, Ord)
-
-newtype NewInformationSet action = NewInformationSet {
-  infoSet :: Set (NewHistory action)
-  } deriving (Eq, Show, Ord)
-
-type NewInformationMap action = Map (NewChoices action) (NewInformationSet action)
-
