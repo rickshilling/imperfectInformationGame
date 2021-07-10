bb15e9192484abfa363b222c5ac59a39555bb3d9
rickshilling <rickshilling@penguin>
2021-07-09 20:03:28 -0400

    never ending clean up

--- src/GameFunctions.hs
+++ src/GameFunctions.hs
@@ -1,24 +1,12 @@
 module GameFunctions
     (
-    gameTraverse,
-    --drawGameTree,
-    --putStrGameTree,
-    --putStrMaybeGameTree,
-    --getInformationMap,
-    --insertInfoMap,
     getActions,
-    --sameInfoSet,
     _A,
-    --_AofI,
     _P,
     _H,
-    --_Z,
     getSetOfInfoSets,
     _I,
-    --filterInfoSetByPlayer,
-    pureSet,
     drawGameTree,
-    --compareAction,
     stepTree,
     drawMaybeGameTree,
     traverseTree,
@@ -31,75 +19,7 @@ import qualified Data.Map as DM
 import qualified Data.Set as DS
 import qualified Data.List as DL
 import qualified Data.Tree as DT
-
 import qualified Control.Monad.State as CMS
---import Control.Monad.State
-
-gameTraverse :: (Eq action) =>
-  (GameTree player action) -> [action] -> Maybe (GameTree player action)
-gameTraverse t [] = Just t
-gameTraverse (GameNode p []) (refA:as) = Nothing
-gameTraverse (GameNode p ((currA, Nothing):rForests)) (refA:as) =
-  if currA == refA then Nothing
-  else gameTraverse (GameNode p rForests) (refA:as)
-gameTraverse (GameNode p ((currA,Just mTree):rForests)) (refA:as) =
-  if currA == refA then gameTraverse mTree as
-  else gameTraverse (GameNode p rForests) (refA:as)
-{-
-drawGameTree :: (Show player, Show action) =>
-  (GameTree player action) -> [String]
-drawGameTree (GameNode p forest) = (lines (show p)) ++ (drawSubTrees forest)
-  where
-    drawSubTrees [] = []
-    drawSubTrees [(a, Nothing)] =  (show a) : []
-    drawSubTrees [(a, Just gt)] =
-      "|" : (show a) : shift "`-" "   " (drawGameTree gt)
-    drawSubTrees ((a, Nothing):restForest) = (show a) : drawSubTrees restForest
-    drawSubTrees ((a, Just gt):restForest) =
-      "|" : (show a) : shift "+-" "|  " (drawGameTree gt) ++ (drawSubTrees restForest)
-    shift first other = zipWith (++) (first : repeat other)
-
-putStrGameTree :: (Show player, Show action) =>
-  (GameTree player action)  -> IO ()
-putStrGameTree g = putStr $ (unlines . drawGameTree) g
-
-putStrMaybeGameTree :: (Show player, Show action) =>
-  Maybe (GameTree player action) -> IO ()
-putStrMaybeGameTree Nothing = putStrLn "Nothing"
-putStrMaybeGameTree (Just gt) = putStrGameTree gt
--}
-
-{-
-getInformationMap :: (Show player, Show action, Ord action) =>
-  (GameTree player action) -> (InformationMap action)
-getInformationMap tree = traverseHelp DM.empty (GameTypes.subForest tree) []
-  where
-  traverseHelp infoMap forest actions = Prelude.foldl (moreHelp actions) (insertInfoMap forest actions infoMap) forest
-  moreHelp actions infoMap (action, Nothing) = DM.insertWith DS.union DS.empty (DS.singleton (actions++[action])) infoMap
-  moreHelp actions infoMap (action, Just tree) = traverseHelp infoMap (GameTypes.subForest tree) (actions++[action])
-
-insertInfoMap :: (Show player, Show action, Ord action) =>
-  [(action, Maybe (GameTree player action) )] -> [action] -> (InformationMap action) -> (InformationMap action)
-insertInfoMap forest actions infoMap = DM.insertWith DS.union (getActions forest) (DS.singleton actions) infoMap
-
-getActions :: (Show player, Show action, Ord action) =>
-  [(action, Maybe (GameTree player action))] -> DS.Set action
-getActions = Prelude.foldl (\set -> \element -> DS.union set (DS.singleton (fst element))) DS.empty
-
-sameInfoSet :: (Show player, Show action, Ord action) =>
-  (GameTree player action) -> History action -> History action -> Bool
-sameInfoSet tree a1 a2 = helper (gameTraverse tree a1) (gameTraverse tree a2)
-  where
-  helper Nothing   Nothing   = True
-  helper Nothing   _         = False
-  helper _         Nothing   = False
-  helper (Just t1) (Just t2) = (getActions (GameTypes.subForest t1)) == (getActions (GameTypes.subForest t2))
-
-_AofI :: (Show player, Show action, Ord action) =>
-  (GameTree player action) -> InformationSet action -> Maybe (DS.Set action)
-_AofI g infoSet = _A g (DS.elemAt 0 infoSet)
--}
-
 
 _H :: (Show action, Ord action) => (InformationMap action) -> DS.Set (History action)
 _H infoMap = Prelude.foldl DS.union DS.empty (DM.elems infoMap)
@@ -121,21 +41,14 @@ _I infoSets history = helper (DS.toList infoSets) history
   helper (is:iss) hs = if DS.member hs is then Just is else helper iss hs
 
 -- Gets information sets of a given player
+{-
 _II_i :: (Show player, Show action, Ord action) =>
   (GameTree player action) -> DS.Set (InformationSet action) -> player -> DS.Set (InformationSet action)
 _II_i g inSets i = DS.foldl (\outSets -> \infoSet -> help g i outSets infoSet) DS.empty inSets
   where
   help g i outSets infoSet = DS.foldl (\outInfoSet -> \h -> help2 g i h outSets) DS.empty infoSet
   help2 g i h infoSet = undefined --_P g
-{-
-filterInfoSetByPlayer :: (Show player, Show action, Ord action, Eq player) =>
-  (GameTree player action) -> player -> InformationSet action -> InformationSet action
-filterInfoSetByPlayer g p infoSet = DS.filter (\h -> (_P g h) == (Just p)) infoSet
 -}
-pureSet :: Maybe (DS.Set a) -> DS.Set a
-pureSet Nothing = DS.empty
-pureSet (Just set) = set
-
 maybeToSet :: Maybe a -> DS.Set a
 maybeToSet Nothing = DS.empty
 maybeToSet (Just e) = DS.singleton e
@@ -143,7 +56,6 @@ maybeToSet (Just e) = DS.singleton e
 maybeToList :: Maybe a -> [a]
 maybeToList Nothing = []
 maybeToList (Just e) = [e]
---
 
 showTreeElement :: (Show player, Show action) => TreeElement player action -> String
 showTreeElement = show
@@ -154,11 +66,6 @@ drawGameTree gt = putStr $ DT.drawTree $ fmap showTreeElement gt
 drawMaybeGameTree :: (Show player, Show action) => Maybe (DT.Tree (TreeElement player action)) -> IO ()
 drawMaybeGameTree (Just gt) = drawGameTree gt
 drawMaybeGameTree Nothing = return ()
-
-{-
-compareAction :: (Eq action) => DT.Tree (TreeElement player action) -> action -> Maybe Bool
-compareAction t a = liftM (== a) (fromAction $ DT.rootLabel t)
--}
 
 stepTree :: (Eq action) => DT.Tree (TreeElement player action) -> action -> Maybe (DT.Tree (TreeElement player action))
 stepTree gt a = DL.find (\t -> (Just a == (fromAction $ DT.rootLabel t)) ) (DT.subForest gt)
@@ -187,11 +94,6 @@ buildInfoMap (DT.Node element forest) = do
   (_,infoMap'') <- CMS.get
   CMS.put (value, infoMap'')
   return ()
-
-getTemp :: Ord action => Game player action -> Int
-getTemp game = 5
-  where
-    tt = fromAction . DT.rootLabel . getTree $ game
 
 getInfoMap :: (Ord action) => DT.Tree (TreeElement player action) -> CMS.State (History action, InformationMap action) ()
 getInfoMap (DT.Node element forest) = do
--- src/GameTypes.hs
+++ src/GameTypes.hs
@@ -1,54 +1,29 @@
 module GameTypes
   (
-  GameTree(..),
   History(..),
-  Sigma(..),
   InformationSet(..),
   InformationMap(..),
-  GameState(..),
-  TreeElement(..),
-  Game(..),
+  Sigma(..),
+  TreeElement(..)
   ) where
-import Control.Monad
 import Data.Map
 import Data.Set
-import qualified Data.Set as DS
-import Control.Monad.State hiding (State)
-import Data.Tree
 
-data GameTree player action = GameNode {
-  rootLabel :: player,
-  subForest :: [(action, Maybe (GameTree player action))]
-}
 type History action = [action]
---type InformationSet action = Set (History action)
---type InformationMap action = Map (Set action) (InformationSet action)
 type InformationSet action = Set (History action)
 type InformationMap action = Map (Set action) (InformationSet action)
-
 type Sigma action = History action -> action -> Float
-instance (Eq player, Eq action) => Eq (GameTree player action) where {
-  GameNode r1 sf1 == GameNode r2 sf2 = (r1 == r2 && sf1 == sf2)
-};
-
-data GameState player action = GameState {
-  location :: History action,
-  game :: GameTree player action
-}
 
 data TreeElement player action = TreeElement {
   getPlayer  :: Maybe player,
   fromAction :: Maybe action
-} 
+}
 
 instance (Show player, Show action) => Show (TreeElement player action) where {
   show (TreeElement p a) = show p ++ ", " ++ show a
 };
 
 instance (Eq player, Eq action) => Eq (TreeElement player action) where {
-  TreeElement p1 a1 == TreeElement p2 a2 = (p1==p2) && (a1==a2)
-  };
+  TreeElement p1 a1 == TreeElement p2 a2 = (p1 == p2) && (a1 == a2)
+};
 
-data Game player action = Game {
-  getTree :: Tree (TreeElement player action)
-}
